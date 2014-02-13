47695ad29be501c9b347a09cdc4d2761ef175ba2
Anurag <anurag3rdsep@gmail.com>
2014-02-13 18:02:54 +0530

    added

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import re
 from flask import Flask, jsonify, request, render_template, json
 app = Flask(__name__, static_folder='static', template_folder= 'templates')
 
-#app.secret_key= '\ybf\xb10\x94au\x2f\xf9\se2\x3f\x93\x26(\xdf\xe4\xaf\x1f\x86k\xb3\x2fQ%1'
+app.secret_key= '\ybf\xb10\x94au\x2f\xf9\se2\x3f\x93\x26(\xdf\xe4\xaf\x1f\x86k\xb3\x2fQ%1'
 
 
 @app.route('/home')
