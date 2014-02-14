5cd5a462c3122c2df3cb7c77212282f1ea5a04c5
Anurag <anurag3rdsep@gmail.com>
2014-02-14 12:50:49 +0530

    change

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__, static_folder='static', template_folder= 'templates')
 app.secret_key= '\ybf\xb10\x94au\x2f\xf9\se2\x3f\x93\x26(\xdf\xe4\xaf\x1f\x86k\xb3\x2fQ%1'
 
 
-@app.route('/home')
+@app.route('/')
 def index():
 	
 	return render_template('home.html')
